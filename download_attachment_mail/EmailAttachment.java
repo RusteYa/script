2745eb991c2309f5b468e14d00b57878dabd86ba
Rustem Yamikov <jarustem@gmail.com>
2016-10-24 17:14:37 +0300

    download mail 0,9

--- download_attachment_mail/EmailAttachment.java
+++ download_attachment_mail/EmailAttachment.java
@@ -1,3 +1,5 @@
+package download_attachment_mail;
+
 import javax.mail.search.FlagTerm;
 import java.io.File;
 import java.io.UnsupportedEncodingException;
@@ -16,7 +18,7 @@ import javax.mail.internet.MimeBodyPart;
 import javax.mail.internet.MimeUtility;
 
 public class EmailAttachment {
-    static String buffer;
+    private static String[] directories;
 
     public static void checkConnect(String host, String storeType, String user, String password1){
         try {
@@ -30,38 +32,28 @@ public class EmailAttachment {
             store.connect(host, user, password1);
             Folder emailFolder = store.getFolder("INBOX");
             emailFolder.open(Folder.READ_WRITE);
-            Flags seen = new Flags(Flags.Flag.SEEN);
+            Flags flag = new Flags(Flags.Flag.FLAGGED);
 
+            FlagTerm unFlag = new FlagTerm(flag, false);
 
-            //создадим видимый флаг
-            //FlagTerm seenFlag = new FlagTerm(seen, true);
+            //find unmarked messages
+            Message messages[] = emailFolder.search(unFlag);
 
+            if (messages.length == 0) {
+                System.out.println("UNMARKED MESSAGES NOT FOUND");
+                System.in.read();
+                System.exit(0);
+            }
 
-            FlagTerm unseenFlag = new FlagTerm(seen, false);
+            directories = new String[messages.length];
 
-//find unread messages
-            Message messages[] = emailFolder.search(unseenFlag);
-
-
-            //ищем прочитанные письма
-            //Message messages[] = emailFolder.search(seenFlag);
-
-            //if (messages.length == 0) {
-            //    System.out.println("UNREAD MESSAGES NOT FOUND");
-            //   System.in.read();
-            //    System.exit(0);
-            //}
-
-            System.out.println("UNREAD MESSAGES : " + emailFolder.getUnreadMessageCount());
-
-            //количество сообщений
-            //System.out.println("READ MESSAGES : " + emailFolder.getMessageCount());
+            System.out.println("UNMARKED MESSAGES : " + messages.length);
 
             String saveDir = System.getProperty("user.home");
             File isDir = new File(saveDir + "\\download_mail");
 
             if(isDir.exists() && (isDir.isDirectory())){
-//dir write?
+                //dir write?
                 if (!isDir.canWrite()) {
                     System.out.println("Access to the directory denied");
                     System.in.read();
@@ -70,7 +62,7 @@ public class EmailAttachment {
             }
 
             else {
-//create dir
+                //create dir
                 boolean makeDir =  isDir.mkdir();
                 if (!makeDir){
                     System.out.println("Error creating directory");
@@ -79,12 +71,13 @@ public class EmailAttachment {
                 }
             }
 
-            for (int i = 0, n = messages.length; i < n; i++) {
-
+            for (int i = 0; i < messages.length; i++) {
                 Message message = messages[i];
                 Object content = message.getContent();
-//no content
-                if (!(content instanceof Multipart)){message.setFlag(Flags.Flag.SEEN, false); continue;}
+
+                message.setFlag(Flags.Flag.FLAGGED, true);
+                //no content
+                if (!(content instanceof Multipart)){continue;}
 
                 Multipart multiPart = (Multipart) content;
                 int numberOfParts = multiPart.getCount();
@@ -93,14 +86,14 @@ public class EmailAttachment {
 
                     MimeBodyPart part = (MimeBodyPart) multiPart.getBodyPart(partCount);
                     if (Part.ATTACHMENT.equalsIgnoreCase(part.getDisposition())) {
-//message.setFlag(Flags.Flag.DELETED, true);
+
                         String fileName = part.getFileName();
                         String  saveFileName = MimeUtility.decodeText(fileName);
 
                         try{
 
-                            buffer = saveFileName;
-                            System.out.println(buffer);
+                            directories[i] = saveFileName;
+                            System.out.println(directories[i]);
 
                             part.saveFile(saveDir + "\\download_mail\\" + saveFileName);
                         }
@@ -110,7 +103,7 @@ public class EmailAttachment {
                     }
                 }
             }
-//close store,folder
+            //close store,folder
             emailFolder.close(false);
             store.close();
 
@@ -124,24 +117,28 @@ public class EmailAttachment {
     }
 
     public static void main(String args[]) throws MessagingException, UnsupportedEncodingException {
-        String host = "imap.gmail.com";//
+        String host = "imap.gmail.com";
         String mailStoreType = "imap";
-        String username = "itisstream2016@gmail.com";//
-        String password1 = "ItIsStream1";//
+        String username = "itisstream2016@gmail.com";
+        String password1 = "ItIsStream1";
 
-        String subject = "Subject";
-        String content = "Test";
+        String subject = "Last fils";
+        String content = "Last unmarked mails";
         String smtpHost="smtp.gmail.com";
-        String addressFrom="user@gmail.com";             String addressTo="user@mail.ru";
+        String addressFrom="userFrom@gmail.com";
+        String addressTo="userTo@mail.ru";
         String login="user@gmail.com";
         String password2="password";
         String smtpPort="587";
 
         checkConnect(host, mailStoreType, username, password1);
 
-        System.out.println(buffer);
-
-        String attachment = System.getProperty("user.home") + "\\download_mail\\" + buffer;
-        TestMail.sendMultiMessage(login, password2, addressFrom, addressTo, content, subject, attachment, smtpPort, smtpHost);
+        for (int i = 0; i < directories.length; i++){
+            if (directories[i] != null){
+                String attachment = System.getProperty("user.home") + "\\download_mail\\" + directories[i];
+                TestMail.sendMultiMessage(login, password2, addressFrom, addressTo, content, subject, attachment, smtpPort, smtpHost);
+                System.out.println(directories[i]);
+            }
+        }
     }
 }
